a7377492fb36bf81555797857c5bd09dd278a962
Carlos Vinicius <carlosviniciustsi@outlook.com>
2022-07-17 10:24:48 -0300

    API test

---
+++ backend/src/main/java/com/carvin/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.carvin.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.carvin.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
