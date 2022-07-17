c1ab85f965360f8812c3bb9980fdf8fc6951d068
Carlos Vinicius <carlosviniciustsi@outlook.com>
2022-07-16 23:54:22 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/carlos.vinicius99/">@carlos.vinicius99</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
